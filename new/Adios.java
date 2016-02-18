7d0882b383dec15e7fbde7a1d383529eb4ec1820
jordi <jordi.gomezs@atos.net>
2016-02-18 12:15:53 +0100

    Command line changes

--- new/Adios.java
+++ new/Adios.java
@@ -3,7 +3,7 @@ public class Adios {
 	private static String nombre = "Name";
 	
 	public static void main(String[] zz){
-
+// comments on Adios mundo
 		System.out.println("Adios mundo " + nombre);
 
 	}
--- new/Hola.java
+++ new/Hola.java
@@ -3,7 +3,7 @@ public class Hola {
 	private static String nombre = "Pato Pajaroto in the master Maneeel"; // merged
 	
 	public static void main(String[] zz){
-
+// comments on Hola Mundo
 		System.out.println("Hola mundo " + nombre);
 
 	}
